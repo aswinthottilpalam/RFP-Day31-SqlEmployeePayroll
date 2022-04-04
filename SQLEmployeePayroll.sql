240bf975963ad80e2e7e4abcab1f02787fcdabd7
aswinthottilpalam <aswinp264@gmail.com>
2022-04-04 12:29:32 +0530

    MS Sql employee payroll practice problem

--- SQLEmployeePayroll.sql
+++ SQLEmployeePayroll.sql
@@ -82,6 +82,61 @@ from Employee_Payroll
 
 
 
+/********* UC8 **********/
+
+Alter Table Employee_Payroll
+ADD Phone_Number VarChar(12)
+
+Alter Table Employee_Payroll
+ADD Department varchar(30)
+
+Alter Table Employee_Payroll
+ADD Emp_address varchar(30)
+
+select *
+from Employee_Payroll
+Go
+
+
+
+/********* UC9 **********/
+
+
+Alter Table Employee_Payroll
+ADD Basic_Pay Money
+
+Alter Table Employee_Payroll
+ADD Deductions Money
+
+Alter Table Employee_Payroll
+ADD Taxable_Pay Money
+
+Alter Table Employee_Payroll
+ADD Income_Tax Money
+
+Alter Table Employee_Payroll
+ADD Net_Pay Money
+
+select *
+from Employee_Payroll
+Go
+
+
+/********* UC10 **********/
+
+insert into Employee_Payroll
+	(Name, Salary, StartDate, Gender, Phone_Number, Department, Emp_address, Basic_Pay, Deductions, Taxable_Pay, Income_Tax, Net_Pay)
+	values ('Terissa', 17500, GETDATE(), 'F', 9998887770, 'Sales', 'Kozhikode',20000, 1000, 19000, 1500, 17500 );
+
+insert into Employee_Payroll
+	(Name, Salary, StartDate, Gender, Phone_Number, Department, Emp_address, Basic_Pay, Deductions, Taxable_Pay, Income_Tax, Net_Pay)
+	values ('Terissa', 22000, GETDATE(), 'F', 9876546547, 'Marketing', 'Trivandrum',25000, 2000, 23000, 1000, 22000 );
+
+select *
+from Employee_Payroll
+Go
+
+
 
 
 
